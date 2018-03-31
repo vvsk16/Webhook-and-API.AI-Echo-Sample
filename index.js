9f24e0107f259f791bf0e0a27192c8cb04975060
vvsk16 <pvvsk77@gmail.com>
2018-04-01 03:40:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res) {
+restService.post("/hit", function(req, res) {
   var speech =
     req.body.result &&
     req.body.result.parameters &&
